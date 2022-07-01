9c825a69c17a6b4b5266a52b5f1c1d64fc32db9d
Daniel Longo <danieldominiclongo@gmail.com>
2022-07-01 09:33:00 -0700

    Create embed-test.js

---
+++ embed-test.js
@@ -0,0 +1,58 @@
+const urlParams = new URLSearchParams(window.location.search);
+const reason = urlParams.get('reason')
+
+console.log("Cookies: " + navigator.cookieEnabled);
+console.log("Browser Language: " + navigator.browserLanguage);
+console.log("Language: " + navigator.language);
+console.log("Platform: " + navigator.platform);
+console.log("Connection Speed: " + navigator.connectionSpeed);
+console.log("User Agent: " + navigator.userAgent);
+console.log("Webdriver: " + navigator.webdriver);
+console.log("Geolocation: " + navigator.geolocation);
+
+var req = new XMLHttpRequest();
+req.open('GET', document.location, false);
+req.send(null);
+
+if (req.status == 404) {
+    Toastify({
+        text: "This page no longer exists. Redirecting now.",
+        duration: 10000,
+        gravity: "top",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+            background: "#6366f1",
+        },
+    }).showToast();
+
+    axios.post('https://linkrotbot.uc.r.appspot.com/mappings/get_redirect', {
+        referrer: document.referrer,
+        URL: document.location.href,
+        // URL: "https://www.madhappy.com/collections/accessories/products/classics-dad-hat?variant=39329359396975"
+    })
+        .then(function (response) {
+        if (response.data.reason != "redirects not enabled") {
+            if (response.data.url.includes("/search?q=")) {
+                location.href = response.data.url + "&reason=" + response.data.reason;    
+            } else {
+                location.href = response.data.url + "?reason=" + response.data.reason;
+            }
+            // location.href = "https://linkrotbottest.myshopify.com/" + "?reason=" + response.data.reason;
+        }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+} else if (reason) {
+    Toastify({
+        text: reason,
+        duration: 3000,
+        gravity: "top",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+            background: "#6366f1",
+        },
+    }).showToast();
+}
